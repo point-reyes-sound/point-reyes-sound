bc57372dcd766b6e3242425898c515b42bd452dd
Romit Chakraborty <romit.chakraborty@gmail.com>
2026-08-29 07:35:16 -0700

    Configure local deck build and add deck/ to .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const inputs = {
+  main: resolve(__dirname, 'index.html'),
+}
+
+// Dynamically include local private slide deck if present
+if (fs.existsSync(resolve(__dirname, 'deck/index.html'))) {
+  inputs.deck = resolve(__dirname, 'deck/index.html')
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: inputs,
+    },
+  },
 })
